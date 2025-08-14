0cbab9db34f98e6c85611c66d2813355c98c5765
lavz <clk.lavanya@gmail.com>
2025-08-15 00:32:53 +0530

    dsdfsdfsdfs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* cuyghuoonfig options here */
+  /* cuyghcxzcxzuoonfig options here */
 };
 
 export default nextConfig;
